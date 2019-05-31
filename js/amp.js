094d7a4d79a112a29d30df119a1650726f8c3a97
Haven <havenicarty@gmail.com>
2019-05-31 15:31:23 +0100

    -ADDED: Texture

--- js/amp.js
+++ js/amp.js
@@ -1,42 +1,40 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );	
 camera.position.z = 100;
 
-// Instantiate a loader
-var loader = new THREE.GLTFLoader();
+
+//var div = document.getElementById('WebGLCanvas');
+
+var loader = new THREE.GLTFLoader(); // Instantiate a loader
 
 // Load a glTF resource
-loader.load( 'models/dsl20c.gltf', function ( gltf ) {
-
-        scene = gltf.scene;
-        //var mesh = scene.children[ 3 ];
+loader.load( '/models/dsl20c.gltf', function ( gltf ) {
 
 		scene.add( gltf.scene );
 
-		//gltf.animations; // Array<THREE.AnimationClip>
-		gltf.scene = scene;
-		//gltf.scenes; // Array<THREE.Scene>
-		gltf.cameras = camera; // Array<THREE.Camera>
-		gltf.asset = 'models/dsl20c.gltf'; // Object
-
-	},
-	// called while loading is progressing
-	function ( xhr ) {
-
+	}, function ( xhr ) { // called while loading is progressing
 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-	},
-	// called when loading has errors
-	function ( error ) {
-
+	}, function ( error ) {
 		console.log( 'An error happened' );
-
 	}
 );
 
-loader.setPath( 'models/dsl20c.gltf');
+//texture
+renderer.gammaOutput = true;
+renderer.gammaFactor = 2.2;
+
+var texture = new THREE.TextureLoader();
+
+texture.load('/Textures/Elephant Grain Black.jpg', function ( texture ) {
+	var material = new THREE.MeshBasicMaterial( { map: texture });
+}, undefined, function ( error ) {
+	console.error ( error );
+});
+
+window.onscroll = (e) => { window.scrollTo(0,0); camera.position.z += 10; console.log(camera.position.z); };
+//material
+//mesh(GLTF, material)
