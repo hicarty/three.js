cf4fae482ed81c8b70aa658015a3637eb12cc76c
Haven <havenicarty@gmail.com>
2019-05-31 11:56:29 +0100

    -ADDED: 3d Model loader

---
+++ js/amp.js
@@ -0,0 +1,41 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+camera.position.z = 5;
+
+// Instantiate a loader
+var loader = new THREE.GLTFLoader();
+
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'models/dsl20c.gltf',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene = scene;
+		gltf.scenes; // Array<THREE.Scene>
+		gltf.cameras = camera; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
