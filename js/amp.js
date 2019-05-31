39d638b248f754b1e15e25c74e263265e4e13c62
Haven <havenicarty@gmail.com>
2019-05-31 12:31:53 +0100

    -TWAEKED: LOADER

--- js/amp.js
+++ js/amp.js
@@ -5,25 +5,24 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-camera.position.z = 5;
+camera.position.z = 100;
 
 // Instantiate a loader
 var loader = new THREE.GLTFLoader();
 
 // Load a glTF resource
-loader.load(
-	// resource URL
-	'models/dsl20c.gltf',
-	// called when the resource is loaded
-	function ( gltf ) {
+loader.load( 'models/dsl20c.gltf', function ( gltf ) {
+
+        scene = gltf.scene;
+        //var mesh = scene.children[ 3 ];
 
 		scene.add( gltf.scene );
 
-		gltf.animations; // Array<THREE.AnimationClip>
+		//gltf.animations; // Array<THREE.AnimationClip>
 		gltf.scene = scene;
-		gltf.scenes; // Array<THREE.Scene>
+		//gltf.scenes; // Array<THREE.Scene>
 		gltf.cameras = camera; // Array<THREE.Camera>
-		gltf.asset; // Object
+		gltf.asset = 'models/dsl20c.gltf'; // Object
 
 	},
 	// called while loading is progressing
@@ -39,3 +38,5 @@ loader.load(
 
 	}
 );
+
+loader.setPath( 'models/dsl20c.gltf');
