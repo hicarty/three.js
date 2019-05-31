9c7799a78e39aaa56e60b13a5f27623d9ca7ee5e
Haven <havenicarty@gmail.com>
2019-05-31 17:29:03 +0100

    -FIXED: Rotation of amp

--- js/amp.js
+++ js/amp.js
@@ -7,8 +7,11 @@ function Main() {
 	const near = 0.1;
 	const far = 100;
 	const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+	camera.position.z = 100;
 
-	camera.position.z = 40;
+	const controls = new THREE.OrbitControls(camera, canvas);
+  	controls.target.set(0, 5, 0);
+  	controls.update();
 
 	const scene = new THREE.Scene();
 	scene.background = new THREE.Color('black');
@@ -43,20 +46,63 @@ function Main() {
 
 	//var div = document.getElementById('WebGLCanvas');
 
-	var loader = new THREE.GLTFLoader(); // Instantiate a loader
+	//OBJECT
+	{
+		var loader = new THREE.GLTFLoader(); // Instantiate a loader
 
-	// Load a glTF resource
-	loader.load( '/models/dsl20c.gltf', function ( gltf ) {
+		// Load a glTF resource
+		loader.load( '/models/dsl20c.gltf', function ( gltf ) {
 			const root = gltf.scene;
-
 			scene.add( root );
+
+			controlOrbit();
 
 		}, function ( xhr ) { // called while loading is progressing
 			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 		}, function ( error ) {
 			console.log( 'An error happened' );
-		}
-	);
+		});
+	}
+
+	function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
+		const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
+		const halfFovY = THREE.Math.degToRad(camera.fov * .5);
+		const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
+		// compute a unit vector that points in the direction the camera is now
+		// in the xz plane from the center of the box
+		const direction = (new THREE.Vector3())
+			.subVectors(camera.position, boxCenter)
+			.multiply(new THREE.Vector3(1, 0, 1))
+			.normalize();
+	
+		// move the camera to a position distance units way from the center
+		// in whatever direction the camera was from the center already
+		camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
+	
+		// pick some near and far values for the frustum that
+		// will contain the box.
+		camera.near = boxSize / 100;
+		camera.far = boxSize * 100;
+	
+		camera.updateProjectionMatrix();
+	
+		// point the camera to look at the center of the box
+		camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
+	}
+
+	function controlOrbit() {
+		// compute the box that contains all the stuff
+		// from root and below
+		const box = new THREE.Box3().setFromObject(root);
+		const boxSize = box.getSize(new THREE.Vector3()).length();
+		const boxCenter = box.getCenter(new THREE.Vector3());
+		// set the camera to frame the box
+		frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
+		// update the Trackball controls to handle the new size
+		controls.maxDistance = boxSize * 10;
+		controls.target.copy(boxCenter);
+		controls.update();
+	}
 
 	//texture
 	renderer.gammaOutput = true;
